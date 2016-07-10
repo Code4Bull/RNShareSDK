4b87be895d0fa06b6a11699562663f946f067db7
Kengsir <kengsir@KengsirdeMacBook-Pro.local>
2016-07-11 00:21:30 +0800

    ios 完善

--- index.ios.js
+++ index.ios.js
@@ -171,3 +171,23 @@ const styles = StyleSheet.create({
 });
 
 AppRegistry.registerComponent('RNShareSDK', () => RNShareSDK);
+
+
+
+<a href="http://www.feng.com/iPhone/news/2016-07-08/Forbes-iPhone-seven-young-benefit-samsung-appearance-changes_651339.shtml"
+   hidefocus="true" target="_blank" title="福布斯：iPhone 7 外观变化小将让三星受益"><i></i>新闻 丨 福布斯：iPhone 7
+    外观变化小将让三星受益</a>
+<a href="http://www.feng.com/iPhone/news/2016-07-08/Supplier-has-for-3-d-glass-shell-for-iPhone-8_651335.shtml"
+hidefocus="true" target="_blank" title="曝供应商已经在为iPhone 8准备3D玻璃外壳"><i></i>新闻 丨 曝供应商已经在为iPhone
+8准备3D玻璃外壳</a>
+<a href="http://www.feng.com/iPhone/news/2016-07-08/popular-Cookie-monster-shortlisted-for-the-ten-best-cooperation-with-Siri-ads_651331.shtml"
+hidefocus="true" target="_blank" title="深得人心 甜饼怪与 Siri 合作的广告入围10佳"><i></i>新闻 丨 深得人心 甜饼怪与 Siri
+合作的广告入围10佳</a>
+<a href="http://www.feng.com/iPhone/news/2016-07-08/After-the-upgrade-with-the-Apple-ID_651327.shtml"
+hidefocus="true" target="_blank" title="升级后用不了Apple ID?iOS 10 Beta 2要接锅"><i></i>新闻 丨 升级后用不了Apple
+ID?iOS 10 Beta 2要接锅</a>
+<a href="http://www.feng.com/iPhone/news/2016-07-08/Apple-and-Google-in-South-Korea-against-the-rules-of-the-uninstall-pre-installed-App_651325.shtml"
+hidefocus="true" target="_blank" title="苹果和谷歌在韩国抵制卸载预装 App 的规定"><i></i>新闻 丨 苹果和谷歌在韩国抵制卸载预装 App
+的规定</a>
+<a href="http://www.feng.com/iPhone/news/2016-07-08/_651322.shtml" hidefocus="true" target="_blank"
+title="在macOS Sierra中 这个快捷键也可开启 Siri"><i></i>新闻 丨 在macOS Sierra中 这个快捷键也可开启 Siri</a>
